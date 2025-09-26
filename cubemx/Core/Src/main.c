71d88596f7b4b154156590b703eedff94086fa38
Franciszek Janicki <franciszek.janicki1@gmail.com>
2025-09-26 10:33:37 +0200

    changes

--- cubemx/Core/Src/main.c
+++ cubemx/Core/Src/main.c
@@ -1,23 +1,25 @@
 /* USER CODE BEGIN Header */
 /**
- ******************************************************************************
- * @file           : main.c
- * @brief          : Main program body
- ******************************************************************************
- * @attention
- *
- * Copyright (c) 2025 STMicroelectronics.
- * All rights reserved.
- *
- * This software is licensed under terms that can be found in the LICENSE file
- * in the root directory of this software component.
- * If no LICENSE file comes with this software, it is provided AS-IS.
- *
- ******************************************************************************
- */
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2025 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
+#include "fatfs.h"
+#include "spi.h"
 #include "usart.h"
 #include "gpio.h"
 
@@ -74,10 +76,8 @@ void SystemClock_Config(void)
 
   /** Configure the main internal regulator output voltage
   */
-  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
-  {
-    Error_Handler();
-  }
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
 
   /** Initializes the RCC Oscillators according to the specified parameters
   * in the RCC_OscInitTypeDef structure.
@@ -87,11 +87,11 @@ void SystemClock_Config(void)
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
   RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
   RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLM = 1;
-  RCC_OscInitStruct.PLL.PLLN = 10;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV7;
-  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
-  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+  RCC_OscInitStruct.PLL.PLLM = 16;
+  RCC_OscInitStruct.PLL.PLLN = 336;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV4;
+  RCC_OscInitStruct.PLL.PLLQ = 2;
+  RCC_OscInitStruct.PLL.PLLR = 2;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     Error_Handler();
@@ -103,10 +103,10 @@ void SystemClock_Config(void)
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_4) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
   {
     Error_Handler();
   }
@@ -123,11 +123,11 @@ void SystemClock_Config(void)
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
-    /* User can add his own implementation to report the HAL error return state
-     */
-    __disable_irq();
-    while (1) {
-    }
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
   /* USER CODE END Error_Handler_Debug */
 }
 
@@ -142,9 +142,8 @@ void Error_Handler(void)
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
-    /* User can add his own implementation to report the file name and line
-       number, ex: printf("Wrong parameters value: file %s on line %d\r\n",
-       file, line) */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
   /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
--- main/main.c
+++ main/main.c
@@ -1,4 +1,9 @@
 #include "main.h"
+#include "fatfs.h"
+#include "gpio.h"
+#include "sd_card.h"
+#include "spi.h"
+#include "usart.h"
 
 void SystemClock_Config(void);
 
@@ -6,4 +11,53 @@ int main(void)
 {
     HAL_Init();
     SystemClock_Config();
+
+    MX_GPIO_Init();
+    MX_USART2_UART_Init();
+    MX_SPI3_Init();
+    MX_FATFS_Init();
+
+    sd_card_config_t card_config = {};
+    strncpy(card_config.mount_point,
+            config->mount_point,
+            sizeof(card_config.mount_point));
+    sd_card_interface_t card_interface = {.file_system = config->filesystem,
+                                          .allocate = pvPortMalloc,
+                                          .deallocate = vPortFree};
+    sd_card_err_t err =
+        sd_card_initialize(&manager->card, &card_config, &card_interface);
+    if (err != SD_CARD_ERR_OK) {
+        return -1;
+    }
+
+    err = sd_card_mount(&manager->card);
+    if (err != SD_CARD_ERR_OK) {
+        printf("Error mounting: %s\n\r", sd_card_err_to_string(err));
+
+        return -1;
+    }
+
+    sd_card_path_t fullpath = "0:/TEST.TXT";
+
+    sd_card_buffer_t write_buffer = {.buffer = "dupa zbita\n\r",
+                                     .buffer_len = strlen("dupa zbita")};
+
+    err = sd_card_write(&manager->card, fullpath, &write_buffer);
+    if (err != SD_CARD_ERR_OK) {
+        printf("Error writing: %s\n\r", sd_card_err_to_string(err));
+
+        return -1;
+    }
+
+    printf("Written to %s: %s\n\r", fullpath, write_buffer.buffer);
+
+    sd_card_buffer_t read_buffer = {.buffer = NULL, .buffer_len = 64};
+    err = sd_card_read(&manager->card, fullpath, &read_buffer);
+    if (err != SD_CARD_ERR_OK) {
+        printf("Error reading: %s\n\r", sd_card_err_to_string(err));
+
+        return -1;
+    }
+
+    printf("Read from %s: %s\n\r", fullpath, read_buffer.buffer);
 }
